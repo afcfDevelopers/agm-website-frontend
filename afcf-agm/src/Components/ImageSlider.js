04b759a6e87723e873508cabfabd77f31a51a643
Sunisaac <isaacayomide2359@gmail.com>
2024-07-29 21:36:15 +0100

    AFCF-AMG

---
+++ afcf-agm/src/Components/ImageSlider.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './ImageSlider.css';
+
+const ImageSlider = () => {
+  const images = [
+    { src: `${process.env.PUBLIC_URL}/slide5.jpeg`, text: 'Apostolic Faith Campus Fellowship Annual General Meeting' },
+    { src: `${process.env.PUBLIC_URL}/slide2.jpeg`, text: 'Apostolic Faith Campus Fellowship Annual General Meeting' },
+    { src: `${process.env.PUBLIC_URL}/slide3.png`, text: 'Apostolic Faith Campus Fellowship Annual General Meeting' },
+    { src: `${process.env.PUBLIC_URL}/slide4.jpeg`, text: 'Apostolic Faith Campus Fellowship Annual General Meeting' },
+    { src: `${process.env.PUBLIC_URL}/slide1.jpeg`, text: 'Apostolic Faith Campus Fellowship Annual General Meeting' },
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    //autoplay: true,
+    autoplaySpeed: 2000,
+  };
+
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index} className="slide">
+            <img src={image.src} alt={`Slide ${index + 1}`} />
+            <div className="overlay">
+              <div className="text">{image.text}</div>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default ImageSlider;
---
+++ afcf-agm/src/Components/Tabs.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import '../Pages/Home/home.css';  // Your custom CSS file
+
+const Tab = ({ tabs, activeCount, inactiveCount} ) => {
+    const [activeTab, setActiveTab] = useState(0);
+
+    return (
+        <div className="tab-container">
+            <div className="tab-buttons">
+                {tabs.map((tab, index) => (
+                    <button
+                        key={index}
+                        className={`tab-button ${activeTab === index ? 'active' : ''}`}
+                        onClick={() => setActiveTab(index)}
+                    >
+                        {tab.label}
+                    </button>
+                ))}
+            </div>
+            <div className='active-campus-container'>
+                <div className='active-campus'> <div className='active-campus-box'></div> <p>Total Active Campus: {activeCount}</p></div>
+                <div className='inactive-campus'> <div className='inactive-campus-box'></div> <p>Total Inactive Campus: {inactiveCount}</p></div>
+              </div>
+            <div className="tab-content">
+                <div style={{overflowX:'auto', width:'100%', height:'100%'}}>
+                {tabs[activeTab].content}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Tab;
